97961a19bc951c0647e63ad5b385dd00c0d2a196
Michal Siminski <michal.siminski3@gmail.com>
2021-06-23 19:19:35 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+interface Task {
+  name: string;
+  date: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-list-solo';
+  title = 'Todo List Michał Simiński';
+
+  taskName: string = '';
+  taskDate: string = '';
+
+  taskList: Task[] = [];
+
+  private clearInputs() {
+    this.taskName = '';
+    this.taskDate = '';
+  }
+
+  createTask() {
+    if (this.taskName !== '' && this.taskDate !== '') {
+      let newTask: Task = {
+        name: this.taskName,
+        date: this.taskDate,
+      };
+      this.clearInputs();
+      this.taskList.push(newTask);
+    }
+  }
+
+  clearTask(task: Task) {
+    this.taskList = this.taskList.filter((e) => e !== task);
+  }
+
+  doneTask(task: Task) {}
 }
